1b41376405b087e2a4d84bfda8ab547e2c8fcaf0
Moksh Patil <mokshpatil42@gmail.com>
2025-03-14 19:43:48 +0530

    created sotryboard boilerplate for project1 - tasks

---
+++ tasks/tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tasks
+//
+//  Created by Moksh Patil on 14/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tasks/tasksTests/tasksTests.swift
@@ -0,0 +1,17 @@
+//
+//  tasksTests.swift
+//  tasksTests
+//
+//  Created by Moksh Patil on 14/03/25.
+//
+
+import Testing
+@testable import tasks
+
+struct tasksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
